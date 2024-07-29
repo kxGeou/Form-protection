b7b5993e97ae6600c01ceaba3eeac28b333c984c
kxGeou <grzegorzm.work@gmail.com>
2024-07-29 19:06:13 +0200

    logic, password showing and hiding

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let registerForm = document.querySelector("form");
 let inputs = document.querySelectorAll(".formControl");
 let submitBtn = document.querySelector("button");
 let statute = document.querySelectorAll(".statuteCheck");
+let showPass = document.querySelector('.showPass');
 
 const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
@@ -13,66 +14,88 @@ const emailR = /^\S+@\S+\.\S+$/;
 registerForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
-
-    const successData = () => {
-        result.innerHTML = `Cześć ${username} ${surname} twój email to ${email} a hasło to ${password}`;
-        statute.forEach(statutes => {
-            statutes.style.color = '#45ff48';
-        })
-    }
-
     let username = inputs[0].value;
     let surname = inputs[1].value;
     let email = inputs[2].value;
     let password = inputs[3].value;
     let repassword = inputs[4].value;    
 
-    if (username == "" ||surname == "" ||email == "" ||password == "" ||repassword == "") {
+
+
+    const error = (innerText, x, y) => {
+        result.innerHTML = innerText;
+        inputs[x].classList.add("formControlWrong");
+        inputs[y].classList.add("formControlWrong");
+    }
+    const errorAll = () => {
         result.innerHTML = "Musisz wypełnić wszystkie pole formularza";
-
-    } else if (username.match(numbers) ||username.match(special) ||surname.match(numbers) ||surname.match(special)) {
-
-        result.innerHTML = "Imie lub Nazwisko zostało źle wypełnione";
-        inputs[1].classList.add("formControlWrong");
-        inputs[0].classList.add("formControlWrong");
-
-    } else if (!email.match(emailR)) {
-        result.innerHTML = "Email został źle wypełniony";
-        inputs[1].classList.remove("formControlWrong");
-        inputs[0].classList.remove("formControlWrong");
-        inputs[2].classList.add("formControlWrong");
-        statute[0].style.color = "#45ff48";
-
-    } else if (!password.match(passwordR)) {
-        inputs[2].classList.remove("formControlWrong");
-        inputs[3].classList.add("formControlWrong");
-        statute[0].style.color = "#45ff48";
-        statute[1].style.color = "#45ff48";
-        result.innerHTML = "Hasło jest źle stworzone";
-
-    } else if (password !== repassword) {
-        inputs[3].classList.remove("formControlWrong");
-        result.innerHTML = "Hasła muszą być identyczne";
-        statute[0].style.color = "#45ff48";
-        statute[1].style.color = "#45ff48";
-        statute[2].style.color = "#45ff48";
-
-    } else {
-        successData()
+        inputs.forEach(input => {
+            input.classList.add("formControlWrong");
+        })
+    }
+    const clearAllStyles = () => {
+        inputs.forEach(input => {
+            input.classList.remove("formControlWrong");
+        })
+    }
+    const correctData = (x,y,z) => {
+        statute[x].style.color = "#45ff48";
+        statute[y].style.color = "#45ff48";
+        statute[z].style.color = "#45ff48";
+    }
+    const successData = () => {
+        clearAllStyles();
+        result.innerHTML = `Cześć ${username} ${surname} twój email to ${email} a hasło to ${password}`;
+        statute.forEach(statutes => {
+            statutes.style.color = '#45ff48';
+        })
         inputs.forEach((input) => {
             input.value = "";
         });
     }
 
 
+    if (username == "" ||surname == "" ||email == "" ||password == "" ||repassword == "") {
+        errorAll();
 
+    } else if (username.match(numbers) ||username.match(special) ||surname.match(numbers) ||surname.match(special)) {
+        clearAllStyles();
+        error('Imie lub Nazwisko zostało źle wypełnione', 1, 0);
 
+    } else if (!email.match(emailR)) {
+        clearAllStyles();
+        error('Email został źle wypełniony', 2);
+        correctData(0)
+
+    } else if (!password.match(passwordR)) {
+        clearAllStyles();
+        error('Hasło jest źle stworzone',3);
+        correctData(0,1)
+
+    } else if (password !== repassword) {
+        clearAllStyles();   
+        error('Hasła muszą być identyczne', 3,4);
+        correctData(0,1,2);
+
+    } else {
+        successData()
+    }
 });
 
+const passShow = () => {
+    inputs[3].type = 'text';
+    inputs[4].type = 'text';
+    showPass.classList.remove('fa-eye-slash')
+    showPass.classList.add('fa-eye')
+    console.log("pokazane")
+}
+const passHidden = () => {
+    inputs[3].type = 'password';
+    inputs[4].type = 'password';
+    showPass.classList.add('fa-eye-slash')
+    showPass.classList.remove('fa-eye')
+    console.log("nie pokazane")
+}
 
-
-/* ZADANIA NA JUTRO 
-    - Stworzenie przełącznika na hasło 
-    - Wyświetlenie komunikatu sukces z ludzikiem
-    - Nadanie wiekszej logiki
-*/
+showPass.addEventListener('mousedown', passShow);
+showPass.addEventListener('mouseup', passHidden);
